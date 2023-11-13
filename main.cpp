0457fcfd7dd43d5cb759cc295f7a1de60b4ffb3a
Tommy Le <tommyle1624@gmail.com>
2023-11-12 22:35:51 -0800

    Update and rename main (2).cpp to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,5 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-#include <iostream>
-#include <string>
+#include <iostream> //cout and cin
+#include <string> //needed to use string
 
 using namespace std;
 
@@ -30,6 +22,7 @@ int main()
     
     //using the instance of the class to use sum
     cout << answer.sum(num1,num2);
+    
     // auto solution = std::to_string(answer.sum);
     // cout << answer.sum;
 
